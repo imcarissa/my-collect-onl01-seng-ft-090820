3ebba56d21908e9979575d0aa218631f8953e422
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-15 17:10:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(languages)
   i = 0
   languages = []
-    while i < array.length
-    collection << yield(array[i])
+    while i < languages.length
+    collection << yield(languages[i])
       i +=1
   end
 end
