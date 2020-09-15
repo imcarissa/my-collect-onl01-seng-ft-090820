63c2d87343568134aa7562f069e1a4fde9af8f07
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-15 16:52:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
 def my_collect(languages)
   i = 0
+  languages = []
+  
+  
 end
 
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
+my_collect(langauges) do |lang|
+  lang.upcase
 end
 
